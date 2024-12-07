6d589d90768ea5b3ea037fd273c7af61029fed28
DURRVenture <durrdurrgamer@gmail.com>
2024-12-06 23:08:00 -0600

    feature: 🌱 initial source

---
+++ src/main/java/net/dotventu/testmod/TestModClient.java
@@ -0,0 +1,12 @@
+package net.dotventu.testmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TestModClient implements ClientModInitializer {
+
+	@Override
+	public void onInitializeClient() {
+
+	}
+
+}
